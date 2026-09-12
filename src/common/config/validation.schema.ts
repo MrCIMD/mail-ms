34a2745cbe6ab887e28611c34205cfe74a7ad1a9
Caleb Isaac Mora Diaz <calebmoradev@gmail.com>
2026-09-12 17:57:31 -0500

    feat: add RabbitMQ service configuration and update validation schema for environment variables

--- src/common/config/validation.schema.ts
+++ src/common/config/validation.schema.ts
@@ -6,5 +6,5 @@ export const validationSchema = joi.object({
   IAM_SECRET_KEY: joi.string().required(),
   AWS_REGION: joi.string().required(),
   IDENTITY_NAME: joi.string().required(),
-  TEMPLATE_DIR: joi.string().required(),
+  TEMPLATE_DIR: joi.string().default('/usr/app/templates'),
 });
